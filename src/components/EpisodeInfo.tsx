2babbf8b0deac14b64383b665aa6bc3ded632d3a
kTrzcinskii <kt.trzcinski@gmail.com>
2023-04-15 23:54:20 +0200

    Make EpisodesSection responsive

--- src/components/EpisodeInfo.tsx
+++ src/components/EpisodeInfo.tsx
@@ -1,5 +1,10 @@
+import { useViewport } from "../hooks/useViewport";
+import { DESKTOP_WIDTH } from "../utils/constants";
+
 const Seperator: React.FC = () => {
-  return <hr className='w-full b-0 bt-[0.063rem] border-t-gray-light' />;
+  return (
+    <hr className='w-full b-0 bt-[0.063rem] border-t-gray-light lg:w-[0.063rem] lg:h-full lg:bg-gray-light ' />
+  );
 };
 
 export type Episode = {
@@ -20,20 +25,28 @@ const EpisodeInfo: React.FC<EpisodeInfoProps> = ({
   index,
   showSeperator = true,
 }) => {
+  const { width } = useViewport();
   return (
-    <li className='w-full relative flex flex-col space-y-[1.125rem]'>
-      <article className='w-full font-bold space-y-[0.563rem]'>
-        <p className='text-[1.125rem]'>{episode}</p>
-        <h3
-          className={`text-[1.125rem] ${
-            index % 2 === 0 ? "text-blue" : "text-green-dark"
-          }`}
-        >
-          {name}
-        </h3>
-        <p className='text-[0.938rem] text-gray'>{airDate}</p>
+    <li className='w-full relative flex flex-col space-y-[1.125rem] lg:flex-row '>
+      <article className='w-full font-bold space-y-[0.563rem] flex flex-col lg:flex-row lg:space-y-0 lg:min-h-[6.625rem]'>
+        <p className='text-[1.125rem] lg:text-[1.563rem] lg:mr-[2.125rem] lg:mt-[1rem]'>
+          {episode}
+        </p>
+        {showSeperator && width >= DESKTOP_WIDTH && <Seperator />}
+        <div className='space-y-[0.563rem] lg:ml-[1.813rem]'>
+          <h3
+            className={`text-[1.125rem] ${
+              index % 2 === 0 ? "text-blue" : "text-green-dark"
+            } lg:text-[1.563rem] lg:mt-[1rem]`}
+          >
+            {name}
+          </h3>
+          <p className='text-[0.938rem] text-gray lg:text-[1.25rem]'>
+            {airDate}
+          </p>
+        </div>
       </article>
-      {showSeperator && <Seperator />}
+      {showSeperator && width < DESKTOP_WIDTH && <Seperator />}
     </li>
   );
 };
--- src/components/EpisodesSection.tsx
+++ src/components/EpisodesSection.tsx
@@ -1,3 +1,5 @@
+import { useViewport } from "../hooks/useViewport";
+import { DESKTOP_WIDTH } from "../utils/constants";
 import EpisodeInfo, { Episode } from "./EpisodeInfo";
 
 const EpisodesSection: React.FC = () => {
@@ -15,55 +17,61 @@ const EpisodesSection: React.FC = () => {
     },
     {
       airDate: "November 10, 2019",
-      episode: "S04E01",
+      episode: "S04E03",
       name: "Edge of Tomorty: Rick, Die, Rickpeat",
     },
     {
       airDate: "November 17, 2019",
-      episode: "S04E02",
+      episode: "S04E04",
       name: "The Old Man and the Seat",
     },
     {
       airDate: "November 10, 2019",
-      episode: "S04E01",
+      episode: "S04E05",
       name: "Edge of Tomorty: Rick, Die, Rickpeat",
     },
     {
       airDate: "November 17, 2019",
-      episode: "S04E02",
+      episode: "S04E06",
       name: "The Old Man and the Seat",
     },
     {
       airDate: "November 10, 2019",
-      episode: "S04E01",
+      episode: "S04E07",
       name: "Edge of Tomorty: Rick, Die, Rickpeat",
     },
     {
       airDate: "November 17, 2019",
-      episode: "S04E02",
+      episode: "S04E08",
       name: "The Old Man and the Seat",
     },
     {
       airDate: "November 10, 2019",
-      episode: "S04E01",
+      episode: "S04E09",
       name: "Edge of Tomorty: Rick, Die, Rickpeat",
     },
     {
       airDate: "November 17, 2019",
-      episode: "S04E02",
+      episode: "S04E10",
       name: "The Old Man and the Seat",
     },
   ];
 
+  const { width } = useViewport();
+
   return (
-    <section className='w-full max-w-[22.625rem] relative mt-[3.5rem]'>
-      <ul className='space-y-[0.938rem]'>
+    <section className='w-full max-w-[22.625rem] relative mt-[3.5rem] lg:mt-[0.375rem] lg:max-w-[28.125rem] xl:max-w-[44.375rem]'>
+      <ul className='space-y-[0.938rem] lg:space-y-0'>
         {fakeData.map((data, index) => {
           return (
             <EpisodeInfo
               {...data}
-              showSeperator={index !== fakeData.length - 1}
+              showSeperator={
+                (index !== fakeData.length - 1 && width < DESKTOP_WIDTH) ||
+                width >= DESKTOP_WIDTH
+              }
               index={index}
+              key={index}
             />
           );
         })}
