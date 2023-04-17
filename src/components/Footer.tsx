9bd27b8c728752b6925ed354b2c21bedc5987fb4
kTrzcinskii <kt.trzcinski@gmail.com>
2023-04-17 18:03:00 +0200

    Place Footer on the bottom of the page
    Previously, the position of the Footer was done by its "margin-top"
    property. Because of that, when viewport height was more than the height
    of the page content Footer ended up in the middle of the screen. Using
    "position absolute" ensures that the Footer is always on the bottom.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
   return (
-    <footer className='uppercase w-full flex justify-center items-center bg-gray-dark  h-[3.063rem] mt-[4.125rem] lg:h-[4.375rem] lg:mt-[9.375rem]'>
+    <footer className='uppercase flex justify-center items-center bg-gray-dark  h-[3.063rem]  lg:h-[4.375rem] absolute bottom-0 left-0 right-0'>
       <p className='text-white-dark font-bold text-[0.875rem] lg:text-[1rem] mr-[1.313rem] lg:mr-0'>
         lorem ipsum &copy;{new Date().getFullYear()}
       </p>
