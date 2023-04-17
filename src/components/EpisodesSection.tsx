edaf59c7471260e6fef0b91588498f56ff567f70
kTrzcinskii <kt.trzcinski@gmail.com>
2023-04-17 17:43:15 +0200

    Fetch data from the API
    In case of error or loading state a corresponding component is rendered,
    otherwise the data itself is displayed

--- src/components/EpisodesSection.tsx
+++ src/components/EpisodesSection.tsx
@@ -1,82 +1,86 @@
+import { useQuery } from "@apollo/client";
 import { useViewport } from "../hooks/useViewport";
-import { DESKTOP_WIDTH } from "../utils/constants";
-import EpisodeInfo, { Episode } from "./EpisodeInfo";
+import {
+  DESKTOP_WIDTH,
+  EXPECTED_NUMBER_OF_EPISODES,
+  SEASON_CODE,
+} from "../utils/constants";
+import EpisodeInfo, { LoadingEpisodeInfo } from "./EpisodeInfo";
+import GET_EPISODES from "../queries/getEpisodes";
+import { ReactNode } from "react";
 
-const EpisodesSection: React.FC = () => {
-  //only for development process
-  const fakeData: Episode[] = [
-    {
-      airDate: "November 10, 2019",
-      episode: "S04E01",
-      name: "Edge of Tomorty: Rick, Die, Rickpeat",
-    },
-    {
-      airDate: "November 17, 2019",
-      episode: "S04E02",
-      name: "The Old Man and the Seat",
-    },
-    {
-      airDate: "November 10, 2019",
-      episode: "S04E03",
-      name: "Edge of Tomorty: Rick, Die, Rickpeat",
-    },
-    {
-      airDate: "November 17, 2019",
-      episode: "S04E04",
-      name: "The Old Man and the Seat",
-    },
-    {
-      airDate: "November 10, 2019",
-      episode: "S04E05",
-      name: "Edge of Tomorty: Rick, Die, Rickpeat",
-    },
-    {
-      airDate: "November 17, 2019",
-      episode: "S04E06",
-      name: "The Old Man and the Seat",
-    },
-    {
-      airDate: "November 10, 2019",
-      episode: "S04E07",
-      name: "Edge of Tomorty: Rick, Die, Rickpeat",
-    },
-    {
-      airDate: "November 17, 2019",
-      episode: "S04E08",
-      name: "The Old Man and the Seat",
-    },
-    {
-      airDate: "November 10, 2019",
-      episode: "S04E09",
-      name: "Edge of Tomorty: Rick, Die, Rickpeat",
-    },
-    {
-      airDate: "November 17, 2019",
-      episode: "S04E10",
-      name: "The Old Man and the Seat",
-    },
-  ];
-
-  const { width } = useViewport();
-
+const Container: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <section className='w-full max-w-[22.625rem] relative mt-[3.5rem] lg:mt-[0.375rem] lg:max-w-[28.125rem] xl:max-w-[44.375rem]'>
+      {children}
+    </section>
+  );
+};
+
+const EpisodesSection: React.FC = () => {
+  const { width } = useViewport();
+  const { data, loading, error } = useQuery(GET_EPISODES, {
+    variables: { page: 1, episodeName: SEASON_CODE },
+  });
+
+  if (loading) {
+    const arr = new Array(EXPECTED_NUMBER_OF_EPISODES).fill(true);
+    return (
+      <Container>
+        <ul className='space-y-[0.938rem] lg:space-y-0 animate-pulse'>
+          {arr.map((_, index) => {
+            return (
+              <LoadingEpisodeInfo
+                showSeperator={
+                  (index !== data?.episodes?.info?.count! - 1 &&
+                    width < DESKTOP_WIDTH) ||
+                  width >= DESKTOP_WIDTH
+                }
+                index={index}
+                key={index}
+              />
+            );
+          })}
+        </ul>
+      </Container>
+    );
+  }
+
+  if (error || !data) {
+    console.log(error?.message);
+    return (
+      <Container>
+        <h4 className='text-2xl lg:text-4xl font-bold w-full text-center mt-[3.125rem] lg:mt-[18.75rem]'>
+          An error occured
+        </h4>
+        <p className='text-md lg:text-xl w-full text-center'>
+          Failed to load data - please try again later.
+        </p>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
       <ul className='space-y-[0.938rem] lg:space-y-0'>
-        {fakeData.map((data, index) => {
+        {data.episodes?.results?.map((episode, index) => {
           return (
             <EpisodeInfo
-              {...data}
+              airDate={episode?.air_date!}
+              episode={episode?.episode!}
+              index={index}
+              name={episode?.name!}
+              key={episode?.id!}
               showSeperator={
-                (index !== fakeData.length - 1 && width < DESKTOP_WIDTH) ||
+                (index !== data.episodes?.info?.count! - 1 &&
+                  width < DESKTOP_WIDTH) ||
                 width >= DESKTOP_WIDTH
               }
-              index={index}
-              key={index}
             />
           );
         })}
       </ul>
-    </section>
+    </Container>
   );
 };
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const DESKTOP_WIDTH = 1024;
 export const API_ENDPOINT = "https://rickandmortyapi.com/graphql";
 export const SEASON_CODE = "S04";
+export const EXPECTED_NUMBER_OF_EPISODES = 10;
